ffc2a2b88d8db92cd2a4d3297aac045593d5a40d
WarFwog <33812@ma-web.nl>
2022-06-16 12:20:25 +0200

    update

---
+++ CSharp2Opdrachten/CSharp2Opdrachten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp2Opdrachten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
